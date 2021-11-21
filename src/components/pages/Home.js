f79b1870c8ab1243da002610408f4e6286b71425
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-21 17:22:08 -0600

    added a few styling attributes to the about me page, still working on the main landing page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,32 @@
-import React from 'react';
-
-
-
+import React from "react";
+import Macbook from "../../images/macbook.png";
+import Notetaker from "../../images/notetaker.png";
+import Flexbox from "../../images/flexbox.jpeg";
+import ImageButton from "react-image-button";
 
 function Home(props) {
   return (
     <div>
-      <h1>New Component</h1>
+      <div className="container">
+        <img
+          id="projectImg"
+          src={Flexbox}
+          className="img-round"
+          alt="profile of me"
+        ></img>
+        <button class="btn">test</button>
+      </div>
+      <div>
+        <img
+          id="projectImg"
+          src={Notetaker}
+          className="img-round"
+          alt="profile of me"
+        ></img>
+      </div>
     </div>
   );
 }
 
-export default Home; 
+export default Home;
 // Use Title and Wrapper like any other React component – except they're styled
