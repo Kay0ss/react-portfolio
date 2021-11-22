ef14da06d2d5f4d4ebef5057c6900b89eb69a9ef
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-21 22:18:19 -0600

    finalized landing page styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -7,11 +7,13 @@ function Home(props) {
   return (
     <div>
       <div>
+        <div className="parallax"></div>
         <figure className="home-wrapper">
           <h4 className="home-typing-demo">
             Welcome, my name is Jack and I like making cool websites..
           </h4>
         </figure>
+
         <div class="scroll-downs">
           <div class="mousey">
             <div class="scroller"></div>
@@ -20,13 +22,15 @@ function Home(props) {
       </div>
       <div className="img-container">
         <img src={captionMe} className="proj-thumb" alt="profile of me"></img>
-        <a id="thumb-links" href="..."><div className="caption-me">C A P T I O N M E  ( M E )</div></a>
+        <a id="thumb-links" href="..."><div className="thumb-text">C A P T I O N M E  ( M E )</div></a>
       </div>
       <div className="img-container">
         <img src={Notetaker} className="proj-thumb" alt="profile of me"></img>
+        <a id="thumb-links" href="..."><div className="thumb-text">N o t e  t a k e r</div></a>
       </div>
       <div className="img-container">
         <img src={star} className="proj-thumb" alt="profile of me"></img>
+        <a id="thumb-links-dark" href="..."><div className="thumb-text-dark">S t a r&emsp;w a r s&emsp;q u i z</div></a>
       </div>
     </div>
   );
