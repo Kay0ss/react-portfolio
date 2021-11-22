4dc90fc1e7d4039a911eb0ebea77a99142f11181
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-21 18:52:55 -0600

    added a few elements to the about page

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,30 +1,11 @@
 import React from "react";
-import Macbook from "../../images/macbook.png";
 import Notetaker from "../../images/notetaker.png";
 import Flexbox from "../../images/flexbox.jpeg";
 import ImageButton from "react-image-button";
 
 function Home(props) {
   return (
-    <div>
-      <div className="container">
-        <img
-          id="projectImg"
-          src={Flexbox}
-          className="img-round"
-          alt="profile of me"
-        ></img>
-        <button class="btn">test</button>
-      </div>
-      <div>
-        <img
-          id="projectImg"
-          src={Notetaker}
-          className="img-round"
-          alt="profile of me"
-        ></img>
-      </div>
-    </div>
+  
   );
 }
 
