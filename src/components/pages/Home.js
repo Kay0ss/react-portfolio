7300afe47a98ceb3912c532cc9ab14fe03fa4390
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-11-21 19:01:14 -0600

    added a few things for deployement to the package.json

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,29 @@
 import React from "react";
 import Notetaker from "../../images/notetaker.png";
 import Flexbox from "../../images/flexbox.jpeg";
-import ImageButton from "react-image-button";
+
 
 function Home(props) {
   return (
-  
+    <div>
+      <div className="container">
+        <img
+          id="projectImg"
+          src={Flexbox}
+          className="img-round"
+          alt="profile of me"
+        ></img>
+        <button class="btn">test</button>
+      </div>
+      <div>
+        <img
+          id="projectImg"
+          src={Notetaker}
+          className="img-round"
+          alt="profile of me"
+        ></img>
+      </div>
+    </div>
   );
 }
 
