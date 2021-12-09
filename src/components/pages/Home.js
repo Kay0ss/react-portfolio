5f0f6f9db6edd823afd0c7d88389bb9c39a29616
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-12-09 13:35:44 -0600

    fianlized 'recent work' area. populated project info

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 // import Card from "react-bootstrap/Card";
 import trueCrime from "../../images/True-Crime.png";
 import Me from "../../images/me.jpeg";
+import captionMe from "../../images/caption-me.png";
+import Starquiz from "../../images/star-wars-quiz.png";
 
 function Home() {
   return (
@@ -38,16 +40,35 @@ function Home() {
       </div>
 
       <div className="recent-title">My Recent Work</div>
-      <div className="recent-bio">Here are a few of the projects I've worked on recently. Want to see more? <a href="mailto:jackschoolbiz@gmail.com"> Email Me.</a></div>
+      <div className="recent-bio">
+        Here are a few of the projects I've worked on recently. Want to see
+        more? <a href="mailto:jackschoolbiz@gmail.com"> Email Me.</a>
+      </div>
       <div className="projects-container">
-          <img className="trueCrime" src={trueCrime} alt="true crime app"></img>
-        <a href="#" className="button">Visit Website</a>
+        <div className="btn-container">
+          <img src={trueCrime} alt="True Crime app"></img>
+          <p className="title">True Crime Quiz</p>
+          <p className="proj-bio">loremsd fsdfa adsf asdf dafadsf adsf asdf sdaf asdf</p>
+          <div className="overlay"></div>
+          <a href="https://true-crime-quiz.herokuapp.com/" target="_blank"><button className="button">Visit Website ▸</button></a>
+        </div>
 
-          <img className="trueCrime" src={trueCrime} alt="true crime app"></img>
-        <a href="#"></a>
+        <div className="btn-container">
+          <img src={captionMe} alt="Caption Me app"></img>
+          <p className="title">True Crime Quiz</p>
+          <p className="proj-bio">loremsd fsdfa adsf asdf dafadsf adsf asdf sdaf asdf</p>
+          <div className="overlay"></div>
+          <a href="https://captionmeme.herokuapp.com/" target="_blank"><button className="button">Visit Website ▸</button></a>
+        </div>
 
-          <img className="trueCrime" src={trueCrime} alt="true crime app"></img>
-        <a href="#"></a>
+        <div className="btn-container">
+          <img src={Starquiz} alt="Star Wars Quiz app"></img>
+          <p className="title">Character Quiz</p>
+          <p className="proj-bio">loremsd fsdfa adsf asdf dafadsf adsf asdf sdaf asdf</p>
+          <div className="overlay"></div>
+          <a href="https://coleenyart.github.io/which-star-wars-character-are-you/" target="_blank"><button className="button">Visit Website ▸</button></a>
+        </div>
+
 
         {/* <a href="#">
           <img className="trueCrime" src={trueCrime} alt="true crime app"></img>
