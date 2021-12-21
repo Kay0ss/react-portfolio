f890c8bfa6a13cd58a95ffcd5c449018e4ff117a
Jack <85977030+Kay0ss@users.noreply.github.com>
2021-12-20 20:03:08 -0600

    finalized styling

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHtml5, faSass, faBootstrap, faGit, faJs, faReact, faCss3Alt, faFigma } from '@fortawesome/free-brands-svg-icons';
+import { faHtml5, faSass, faBootstrap, faGit, faJsSquare, faReact, faCss3Alt, faFigma, faNodeJs } from '@fortawesome/free-brands-svg-icons';
 import skills from "../../images/Skills.svg";
 // import { react } from '@fortawesome/fontawesome-svg-core';
 
@@ -10,14 +10,16 @@ function Skills(props){
         <div className="skills-section">
         <img src={skills} className="skills-title" alt="skills"></img>
         <div className="skills-container">
-        <h1><FontAwesomeIcon icon={faHtml5} size="2x" className="html5"/></h1>
-        <h1><FontAwesomeIcon icon={faCss3Alt} size="2x" className="css3"/></h1>
-        <h1><FontAwesomeIcon icon={faJs} size="2x" className="js"/></h1>
-        <h1><FontAwesomeIcon icon={faBootstrap} size="2x" className="bootstrap"/></h1>
-        <h1><FontAwesomeIcon icon={faGit} size="2x" className="git"/></h1>
-        <h1><FontAwesomeIcon icon={faReact} size="2x" className="react"/></h1>
-        <h1><FontAwesomeIcon icon={faSass} size="2x" className="sass"/></h1>
-        <h1><FontAwesomeIcon icon={faFigma} size="2x" className="figma"/></h1>
+        <h1><FontAwesomeIcon icon={faHtml5} size="3x" className="html5"/></h1>
+        <h1><FontAwesomeIcon icon={faCss3Alt} size="3x" className="css3"/></h1>
+        <h1><FontAwesomeIcon icon={faJsSquare} size="3x" className="js"/></h1>
+        <h1><FontAwesomeIcon icon={faBootstrap} size="3x" className="bootstrap"/></h1>
+        <h1><FontAwesomeIcon icon={faGit} size="3x" className="git"/></h1>
+        <h1><FontAwesomeIcon icon={faReact} size="3x" className="react"/></h1>
+        <h1><FontAwesomeIcon icon={faSass} size="3x" className="sass"/></h1>
+        <h1><FontAwesomeIcon icon={faFigma} size="3x" className="figma"/></h1>
+        <h1><FontAwesomeIcon icon={faNodeJs} size="3x" className="node"/></h1>
+
 
 
         </div>
