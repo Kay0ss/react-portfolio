8ab44e90c77595275f4dd369374eccd084751a36
Jack <85977030+Kay0ss@users.noreply.github.com>
2022-01-30 10:55:11 -0600

    bug fixes

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -3,30 +3,23 @@ import Projects from "../pages/Projects";
 import About from "../pages/About";
 import Contact from "../pages/Contact";
 import Skills from "../pages/Skills";
-// import GitHubCalendar from "react-github-calendar";
 import Days from "../pages/iframe";
 import landingPage from "../../images/landingpage.svg";
-import { faAngleDoubleDown } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "animate.css/animate.min.css";
 import "animate.css";
 
 function Home() {
+
   return (
     <>
       <div className="landing">
         <img className="hero" src={landingPage} alt="hero" />
       </div>
-
-      {/* <h1 className="bounce"><FontAwesomeIcon icon={ faAngleDoubleDown }></FontAwesomeIcon></h1> */}
       <About />
       <Skills />
       <Days />
       <Projects />
       <Contact id="Contact" />
-
-      {/* <GitHubCalendar username="Kay0ss" /> */}
-
       <div className="copyright"> © 2021 Jack Theisen</div>
     </>
   );
--- src/components/pages/iframe.js
+++ src/components/pages/iframe.js
@@ -3,7 +3,6 @@ import React from "react";
 function Days() {
   return (
     <div className="iframe-container">
-
       <iframe src="https://csb-0g3fh.netlify.app/" title="Days i Code" scrolling="no"></iframe>
     </div>
   );
